b95bad84b63e950bb0516a255c60db9544055309
Huy Hoang <102393576+hhoang91@users.noreply.github.com>
2023-11-14 04:55:28 +0000

    add dynamo_db instance to run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 sh ./bin/create-dotenv.sh
 
 # Start the services
-foreman start -m gateway=1,enrollment_service=3,user_service=1
+foreman start -m gateway=1,enrollment_service=3,user_service=1,dynamo_db=1
