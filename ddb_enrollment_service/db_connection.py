5386cb164db72c7ec1f3619c37382c6b9a17e02f
Huy Hoang <102393576+hhoang91@users.noreply.github.com>
2023-11-24 10:51:26 +0000

    create redis connection

--- ddb_enrollment_service/db_connection.py
+++ ddb_enrollment_service/db_connection.py
@@ -1,11 +1,15 @@
 import boto3
+import redis
 
 def get_db():
 
     dynamodb_resource = boto3.resource(
     'dynamodb',
-    aws_access_key_id='fakeMyKeyId',
-    aws_secret_access_key='fakeSecretAccessKey',
+    #aws_access_key_id='fakeMyKeyId',
+    #aws_secret_access_key='fakeSecretAccessKey',
     endpoint_url='http://localhost:5300'
 )
     return dynamodb_resource
+
+def get_redis_db():
+    return redis.Redis()
