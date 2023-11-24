21a9922ef90b2500ed79b40b90f361f9c568947c
Huy Hoang <102393576+hhoang91@users.noreply.github.com>
2023-11-24 10:48:58 +0000

    registrar router prototype

--- ddb_enrollment_service/registrar_router.py
+++ ddb_enrollment_service/registrar_router.py
@@ -2,7 +2,7 @@ from typing import Annotated
 import boto3
 from fastapi import Depends, Response, HTTPException, Body, status, APIRouter
 from .db_connection import get_db
-from ddb_enrollment_schema import *
+from .ddb_enrollment_schema import *
 from boto3.dynamodb.conditions import Key
 from .models import Course, ClassCreate, ClassPatch
 WAITLIST_CAPACITY = 15
@@ -10,10 +10,35 @@ MAX_NUMBER_OF_WAITLISTS_PER_STUDENT = 3
 
 registrar_router = APIRouter()
 
+@registrar_router.put("/auto-enrollment/")
+def set_auto_enrollment(enabled: Annotated[bool, Body(embed=True)], db: boto3.resource = Depends(get_db)):
+    """
+    Endpoint for enabling/disabling automatic enrollment.
+
+    Parameters:
+    - enabled (bool): A boolean indicating whether automatic enrollment should be enabled or disabled.
+
+    Raises:
+    - HTTPException (409): If there is an integrity error while updating the database.
+
+    Returns:
+        dict: A dictionary containing a detail message confirming the status of auto enrollment.
+    """
+    try:
+        table = db.Table('configs_table')
+        table.put_item(
+            Item={
+                'variable_name': 'automatic_enrollment',
+                'value': enabled,
+        }
+)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error retrieving drop list: {str(e)}")
+
+    return {"detail": f"Auto enrollment: {enabled}"}
+
 @registrar_router.post("/classes/", status_code=status.HTTP_201_CREATED)
-def create_class(
-    body_data: ClassCreate, response: Response
-):
+def create_class(body_data: ClassCreate, db: boto3.resource = Depends(get_db)):
     """
     Creates a new class.
 
@@ -62,4 +87,107 @@ def create_class(
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error creating class: {str(e)}")
+    
+@registrar_router.post("/courses/", status_code=status.HTTP_201_CREATED)
+def create_course(course: Course, db: boto3.resource = Depends(get_db)
+):
+    """
+    Creates a new course with the provided details.
 
+    Parameters:
+    - `course` (CourseInput): JSON body input for the course with the following fields:
+        - `department_code` (str): The department code for the course.
+        - `course_no` (int): The course number.
+        - `title` (str): The title of the course.
+
+    Returns:
+    - dict: A dictionary containing the details of the created item.
+
+    Raises:
+    - HTTPException (409): If a conflict occurs (e.g., duplicate course).
+    """
+    try:
+        table = db.Table('course_table')
+        table.put_item(
+            Item={
+                'department_code': course.department_code,
+                'course_no': course.course_no,
+                'course_name': course.title
+            }
+        )   
+
+        return {"Course created"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error creating course: {str(e)}")
+        
+@registrar_router.delete("/classes/{id}", status_code=status.HTTP_200_OK)
+def delete_class(id: int, db: boto3.resource = Depends(get_db)):
+    """
+    Deletes a specific class.
+
+    Parameters:
+    - `id` (int): The ID of the class to delete.
+
+    Returns:
+    - dict: A dictionary indicating the success of the deletion operation.
+      Example: {"message": "Item deleted successfully"}
+
+    Raises:
+    - HTTPException (404): If the class with the specified ID is not found.
+    - HTTPException (409): If there is a conflict in the delete operation.
+    """
+    # TO DO create a variable name is_deleted, set it to true when calling this api instead of actually deleting the record
+    
+    try:
+        table = db.Table('class_table')
+        table.delete_item(
+            Key={
+                'id': str(id)
+            }
+        )  
+
+        return {"message": "Item deleted successfully"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error creating course: {str(e)}")
+
+@registrar_router.patch("/classes/{id}", status_code=status.HTTP_200_OK)
+def update_class(id: int, body_data: ClassPatch, db: boto3.resource = Depends(get_db)):
+    """
+    Updates specific details of a class.
+
+    Parameters:
+    - `class` (ClassPatch): The JSON object representing the class with the following properties:
+        - `section_no` (int, optional): Section number.
+        - `instructor_id` (int, optional): Instructor ID.
+        - `room_num` (int, optional): Room number.
+        - `room_capacity` (int, optional): Room capacity.
+        - `course_start_date` (str, optional): Course start date (format: "YYYY-MM-DD").
+        - `enrollment_start` (str, optional): Enrollment start date (format: "YYYY-MM-DD HH:MM:SS.SSS").
+        - `enrollment_end` (str, optional): Enrollment end date (format: "YYYY-MM-DD HH:MM:SS.SSS").
+
+    Returns:
+    - dict: A dictionary indicating the success of the update operation.
+      Example: {"message": "Item updated successfully"}
+
+    Raises:
+    - HTTPException (404): If the class with the specified ID is not found.
+    - HTTPException (409): If there is a conflict in the update operation (e.g., duplicate class details).
+    """
+    try:
+        table = db.Table('class_table')
+        table.update_item(
+            Key={
+                'id': str(id),
+            },
+            UpdateExpression='SET instuctor_id = :val1',
+            ExpressionAttributeValues={
+                ':val1': body_data.instructor_id
+            }
+        )       
+
+        return {"message": "Item updated successfully"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error updating item: {str(e)}")
